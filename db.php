c72ebbd1f5dd1226d189d58adfa02eae93993df6
Varad <admin@Admins-MacBook-Pro.local>
2026-03-30 18:15:27 +0530

    Assignment 4 - PHP and MySQL

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+// db.php - Database connection
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "studentdb";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
